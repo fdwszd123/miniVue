4dd50c1ac45fbeeb8d4461c5fb432ad60ea534ee
Fantsy <fdwszd@163.com>
2022-04-21 10:48:29 +0800

    v2响应式

--- reactive/reactive.js
+++ reactive/reactive.js
@@ -2,65 +2,94 @@ class Dep {
   constructor() {
     this.subscribers = new Set();
   }
+
   depend() {
     if (activeEffect) {
       this.subscribers.add(activeEffect);
     }
   }
+
   notify() {
     this.subscribers.forEach((effect) => {
       effect();
     });
   }
 }
-const dep = new Dep();
+
 let activeEffect = null;
-//收集依赖
 function watchEffect(effect) {
   activeEffect = effect;
-  dep.depend();
   effect();
   activeEffect = null;
 }
 
+// Map({key: value}): key是一个字符串
+// WeakMap({key(对象): value}): key是一个对象, 弱引用
 const targetMap = new WeakMap();
 function getDep(target, key) {
-  // 根据对象target取出对应的Map对象
+  // 1.根据对象(target)取出对应的Map对象
   let depsMap = targetMap.get(target);
   if (!depsMap) {
     depsMap = new Map();
     targetMap.set(target, depsMap);
   }
+
+  // 2.取出具体的dep对象
   let dep = depsMap.get(key);
   if (!dep) {
-    dep = new Map();
-    //commit test
-    dep.set(key, dep);
+    dep = new Dep();
+    depsMap.set(key, dep);
   }
+  return dep;
 }
 
-//对数据进行数据劫持 Object.defineProperty
+// vue2对raw进行数据劫持
 function reactive(raw) {
   Object.keys(raw).forEach((key) => {
+    const dep = getDep(raw, key);
+    let value = raw[key];
+
     Object.defineProperty(raw, key, {
       get() {
         dep.depend();
+        return value;
       },
-      set(newVal) {},
+      set(newValue) {
+        if (value !== newValue) {
+          value = newValue;
+          dep.notify();
+        }
+      },
     });
   });
+
   return raw;
 }
 
-//test
-const info = { counter: 100 };
-watchEffect(function doubleCounter() {
-  console.log(info.counter * 2);
+// 测试代码
+const info = reactive({ counter: 100, name: "why" });
+const foo = reactive({ height: 1.88 });
+
+// watchEffect1
+watchEffect(function () {
+  console.log("effect1:", info.counter * 2, info.name);
 });
 
-watchEffect(function powerCounter() {
-  console.log(info.counter * info.counter);
+// watchEffect2
+watchEffect(function () {
+  console.log("effect2:", info.counter * info.counter);
+});
+
+// watchEffect3
+watchEffect(function () {
+  console.log("effect3:", info.counter + 10, info.name);
+});
+
+watchEffect(function () {
+  console.log("effect4:", foo.height);
 });
 
 info.counter++;
-dep.notify();
+// info.name = "why";
+
+// foo.height = 2;
